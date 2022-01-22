dc56c5c0478636cdb3e2251d86f6b3a8ebd73341
tati <miranda.tatianayamila@gmail.com>
2022-01-22 14:30:29 -0300

    final

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const tarjetas = document.querySelector("#cards")
-const busqueda = document.querySelector("#search")
+
 const prev = document.querySelector("#prev")
 const next = document.querySelector("#next")
 const formSection = document.querySelector(".form-section")
 const closebutton = document.querySelector(".close")
 const container = document.querySelector(".container-details")
-const targetSection = document.querySelector("#section-cards")
+const targetSection = document.querySelector(".section-cards")
 const targetDetails = document.querySelector(".section-details")
+const characterButton = document.querySelector(".nav-button")
+const sectionCards = document.querySelector("#cards")
+
+
 
 // Home Page
 let actualPage = 1
@@ -42,6 +45,21 @@ prev.onclick = () => {
     searchInfo()
 }
 
+
+
+//Character Button
+characterButton.onclick = () => {
+    console.log("hola")
+    targetSection.style.display = "flex"
+    sectionCards.style.display = "none"
+    container.style.display = "flex"
+
+}
+//Location Button
+
+
+//Function for all the cards
+
 const cardsAllHTML = (data) => {
     const html = data.reduce((acc, curr) => {
         return acc + `
@@ -57,6 +75,7 @@ const cardsAllHTML = (data) => {
 
 }
 searchInfo()
+
 
 
 
@@ -128,22 +147,22 @@ const view = (data) => {
 
     targetDetails.innerHTML =
         `
-          <div class="cardFull">
+          <div class="card-details">
           <button class="close">✖</button>
           
  <img class="img" src = "${data.image}"> </img>
- <div class="cardOfcards">
+ <div class="card-details-info">
  
-           <h3>${data.name} </h3>
-           <p><b>Status: </b>${data.status} - ${data.species} - ${data.gender}</p>          
+           <h2>${data.name} </h2>
+           <h3><b>Status: </b>${data.status} - ${data.species} - ${data.gender}</h3>          
  
          <div>
-<h4>Firts Seen in:</h4>
+<h3>Firts Seen in:</h3>
 <p> ${data.origin.name}</p>
 </div>
 
 <div>
-<h4>Last Location in:</h4>
+<h3>Last Location in:</h3>
 <p> ${data.location.name}</p>
 </div>
 
@@ -179,3 +198,55 @@ const characterOnly = () => {
     }
 }
 
+
+// const genderInfo = () => {
+//     fetch(`https://rickandmortyapi.com/api/character?gender=${gender}`)
+//         .then((res) => {
+//             return res.json()
+//         })
+//         .then((data) => {
+//             cardOnly(data.results)
+//             characterOnly()
+
+
+//         })
+// }
+
+// const select = document.querySelector("#gender")
+// const option = document.querySelector(".option-gender")
+
+// select.onchange = (e) => {
+//     e.preventDefault();
+//     genderInfo(option.value)
+// }
+
+
+// const genderOnly = () => {
+//     const cardOnlyGender = document.querySelectorAll(".option-gender")
+//     for (let i = 0; i < cardOnlyGender.length; i++) {
+//         cardOnly[i].onchange = () => {
+
+//             const id = cardOnlyGender[i].dataset.id
+//             genderInfo(id)
+
+
+
+
+//         }
+//     }
+// }
+
+
+// const cardOnlyGender = (gender) => {
+//     const html = gender.reduce((acc, curr) => {
+//         return acc + `
+//           <div class="card" data-id="${curr.id}">
+//            <h2>${curr.name}</h2>
+//             <img class="img" src =${curr.image}> </img>
+//            </div>       
+//            `
+//     }, "")
+//     cards.innerHTML = html
+
+
+// }
